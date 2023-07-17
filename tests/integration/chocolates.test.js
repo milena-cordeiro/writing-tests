d7a7f9567f8c7af16453ed89fb12351e42a8d4ac
Milena Cordeiro <mcs9823@gmail.com>
2023-07-17 18:53:37 -0300

    usando sinon e stub nos testes

--- tests/integration/chocolates.test.js
+++ tests/integration/chocolates.test.js
@@ -1,10 +1,60 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
+const sinon = require('sinon');
+const fs = require('fs');
 const app = require('../../src/app');
 
 chai.use(chaiHttp);
 
+const mockFile = JSON.stringify({ 
+  brands: [
+    {
+      id: 1,
+      name: 'Lindt & Sprungli',
+    },
+    {
+      id: 2,
+      name: 'Ferrero',
+    },
+    {
+      id: 3,
+      name: 'Ghirardelli',
+    },
+  ],
+  chocolates: [
+    {
+      id: 1,
+      name: 'Mint Intense',
+      brandId: 1,
+    },
+    {
+      id: 2,
+      name: 'White Coconut',
+      brandId: 1,
+    },
+    {
+      id: 3,
+      name: 'Mon Chéri',
+      brandId: 2,
+    },
+    {
+      id: 4,
+      name: 'Mounds',
+      brandId: 3,
+    },
+  ],
+})
+
 const { expect } = chai;
+
+beforeEach(function () {
+   sinon.stub(fs.promises, 'readFile')
+        .resolves(mockFile);
+});
+
+afterEach(function () {
+  sinon.restore();
+})
 
 describe('Usando GET em /chocolates', function () {
   it('Retorna a lista completa de chocolates!', async function () {
@@ -16,7 +66,7 @@ describe('Usando GET em /chocolates', function () {
     ];
     const response = await chai.request(app).get('/chocolates');
 // verificar se o código HTTP retornado corresponde ao valor esperado:
-    expect(response.status).to.be.equals(200);
+    expect(response.status).to.be.equal(200);
 // validamos se o corpo da mensagem corresponde a lista com todos os chocolates cadastrados na API:
 expect(response.body.chocolates).to.deep.equal(output);
 
